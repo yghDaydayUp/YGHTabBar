b810566612559231a88740503605af18cf201471
yghDaydayUp <ygh7573418@163.com>
2016-05-03 10:31:59 +0800

    Initial Commit

---
+++ YGHTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YGHTabBar
+//
+//  Created by YangGH on 16/5/3.
+//  Copyright © 2016年 YangGH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YGHTabBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YGHTabBar
+//
+//  Created by YangGH on 16/5/3.
+//  Copyright © 2016年 YangGH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
